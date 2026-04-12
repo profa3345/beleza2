9a8c63da0c8c127da4a58dbd57ae1bdc96f92a05
profa3345 <profa3345@gmail.com>
2026-04-12 18:31:48 -0300

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFunctions }     from "firebase/functions";
 import { getStorage }       from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:            "AIzaSyDyL0bxTfe7X2de_O-wbKn09feIIxi4TTs",
+  authDomain:        "beleza-hub.firebaseapp.com",
+  projectId:         "beleza-hub",
+  storageBucket:     "beleza-hub.firebasestorage.app",
+  messagingSenderId: "752625992317",
+  appId:             "1:752625992317:web:1fac364495bbb3c110e714",
 };
 
 const app = initializeApp(firebaseConfig);
