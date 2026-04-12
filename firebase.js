29ec058c6dc0f9635baf8d02e79a67a110e5b341
profa3345 <profa3345@gmail.com>
2026-04-12 17:50:27 -0300

    Remove Firebase setup from firebase.js
    Removed Firebase initialization and configuration code.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDyL0bxTfe7X2de_O-wbKn09feIIxi4TTs",
-  authDomain: "beleza-hub.firebaseapp.com",
-  projectId: "beleza-hub",
-  storageBucket: "beleza-hub.firebasestorage.app",
-  messagingSenderId: "752625992317",
-  appId: "1:752625992317:web:1fac364495bbb3c110e714"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db   = getFirestore(app);
-export const auth = getAuth(app);
